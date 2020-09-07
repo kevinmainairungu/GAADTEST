6da7915af1c98b0fbec0deccb36a48ec1047c05b
kevinmainairungu <mainairungu99@gmail.com>
2020-09-07 23:12:52 +0300

    added timber Logging

--- app/src/main/java/com/example/gaadtest/MainActivity.kt
+++ app/src/main/java/com/example/gaadtest/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.gaadtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timber.i("activity has been created")
     }
+
+
 }
