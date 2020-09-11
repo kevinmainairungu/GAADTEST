d137715d78cd945532d5ea8042daac62220cc70f
kevinmainairungu <mainairungu99@gmail.com>
2020-09-11 11:05:40 +0300

    working on the architecture and code cleanup

--- app/src/main/java/com/example/gaadtest/MODEL/Hours.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gaadtest.MODEL
-
-data class Hours(val name: String, val hours: Int, val country: String, val badgeUrl: String)
-
-
-data class Skills(val name: String, val score: Int, val country: String, val badgeUrl: String)
